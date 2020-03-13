66bb04061cc8c8c0508881d4f3e004855bad87f0
Tommy Shao <Tommy_shao@yahoo.com>
2020-03-13 22:21:08 +0800

    adding some messages

--- app.py
+++ app.py
@@ -38,12 +38,15 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    r = '很抱歉,您說什麼'
+    # r = '很抱歉,您說什麼'
 
     if msg == 'hi':
         r = 'hi'
     elif msg == '你吃飯了嗎?' :
         r = '還沒'
+    else :
+        r = '看不懂'
+
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=r))
