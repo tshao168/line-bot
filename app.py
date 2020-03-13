baac2c0ef55d420d007b812063d64a43c7e3375f
Tommy Shao <Tommy_shao@yahoo.com>
2020-03-13 22:33:26 +0800

    update

--- app.py
+++ app.py
@@ -40,10 +40,14 @@ def handle_message(event):
     msg = event.message.text
     # r = '很抱歉,您說什麼'
 
-    if msg == 'hi':
-        r = 'hi'
+    if msg in ['hi', 'Hi'] :
+        r = '嗨'
     elif msg == '吃飯了嗎' :
         r = '還沒'
+    elif msg == '你是誰' :
+        r = '我是機器人'
+    elif '訂位' in msg :
+        r = '你想訂位嗎?'
     else :
         r = '看不懂'
 
